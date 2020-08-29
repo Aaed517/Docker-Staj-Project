b64ac9d08fe36235696a53012116afa174629ece
Mustafa Onur Çiçekalan <43696440+Aaed517@users.noreply.github.com>
2020-08-29 20:33:50 +0300

    Update settings.py

--- staj-project/staj/settings.py
+++ staj-project/staj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4eem-=gb!z3=l!koq)-7dti4#(2qja%oq1$kg!stmp8^+jc-(0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1','192.168.99.100']
 
 
 # Application definition
